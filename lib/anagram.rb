9afbc62f4ce99cdf8d472d174c043bad60735246
Steven McVey <smcvey1210@gmail.com>
2022-12-11 21:30:02 -0800

    commit tree a478b38a86769298e08d89528b3b27e133ab7741

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word=word
+  end
+
+  def match(string)
+    @sorted_word = word.split("").sort
+    p @sorted_word
+
+      string.select do |test_word|
+        @sorted_word == test_word.split("").sort
+    end
+  end
+end
